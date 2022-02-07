ea9a63d30d3a5ba412c30b1e78accf399e0ea9d6
Charles Peterson <artistan@gmail.com>
2022-02-07 08:43:47 -0600

    buffer # to restrict to alpha characters.

--- scripts/word-length-list.php
+++ scripts/word-length-list.php
@@ -1,20 +1,20 @@
 #!/usr/local/bin/php
 <?php
 
-if(empty($argv[1])) {
+if (empty($argv[1])) {
     echo "provide number\n\n";
     exit;
 }
 $numb = ($argv[1]) * 1;
 
 $fp = fopen(__DIR__ . '/../words.txt', 'r');
-$fpw = fopen(__DIR__ . '/words-' . $numb . '.txt','w');
+$fpw = fopen(__DIR__ . '/words-' . $numb . '.txt', 'w');
 
 if ($fp) {
     while (($buffer = fgets($fp, 4096)) !== false) {
         // only keep alpha character words
-        if(strlen(trim($buffer)) == $numb && preg_match('/[a-z]{5}/i', $buffer)) {
-            fwrite($fpw,strtolower($buffer));
+        if (strlen(trim($buffer)) == $numb && preg_match('/[a-z]{' . $numb . '}/i', $buffer)) {
+            fwrite($fpw, strtolower($buffer));
         }
     }
     if (!feof($fp)) {
